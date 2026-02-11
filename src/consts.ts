dad9d3c78b629cb3faa133dfa499cac4a671daac
falog <fal@falog.net>
2026-02-11 11:21:27 +0900

    description

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'ふぁろぐねっと | 初心者をやめなかった人';
-export const SITE_DESCRIPTION = '英語学習と技術開発の記録。OS開発、アプリ開発、回路設計などを初心者目線でログしています。';
+export const SITE_DESCRIPTION = '言語学習と技術開発の記録。OS開発、アプリ開発、回路設計などを初心者目線でログしています。';
 
