806b2a0661e38a1d3d3e0c73f15282922305c50c
falog <fal@falog.net>
2026-05-17 06:36:48 +0900

    タイトルを見やすいように修正

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ふぁろぐねっと | 初心者をやめなかった人';
-export const SITE_DESCRIPTION = '言語学習と技術開発の記録。OS開発、アプリ開発、回路設計などを初心者目線でログしています。';
-
+export const SITE_TITLE = 'ふぁろぐねっと';
+export const SUB_TITLE = '初心者をやめなかった人';
+export const SITE_DESCRIPTION =
+  '初心者をやめなかった人のための、CPU自作・OS開発・回路設計・Web開発・言語学習の記録ブログ。';
