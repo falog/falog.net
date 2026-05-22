65b6117a1005d24c67055a2794705d7fe139f2b6
falog <fal@falog.net>
2026-05-23 03:10:47 +0900

    記事整理

--- src/utils/category.ts
+++ src/utils/category.ts
@@ -1,5 +1,8 @@
 export const categoryGroups = {
-  cpu: ['cpu', 'riscv'],
+  cpu: ['cpu', 'riscv', 'os'],
   web: ['web'],
-  english: ['english', 'falkoe'],
+  languages: ['english', 'russian'],
+  falkoe: ['falkoe'],
+  wazzefalradio: ['wazzefalradio'],
+  others: ['others', 'daily', 'blog'],
 } as const;
