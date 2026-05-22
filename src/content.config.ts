91531b1dce786e927e538190a9b7c2ed234b7f8d
falog <fal@falog.net>
2026-05-23 03:19:06 +0900

    fix:category

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,7 +10,22 @@ const blog = defineCollection({
       pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
       heroImage: image().optional(),
-      category: z.enum(['about', 'blog', 'cpu', 'riscv','falkoe']),
+      category: z
+        .enum([
+          'about',
+          'blog',
+          'cpu',
+          'riscv',
+          'falkoe',
+          'web',
+          'english',
+          'russian',
+          'os',
+          'wazzefalradio',
+          'others',
+          'daily',
+        ])
+        .optional(),
       order: z.number().optional(),
       tags: z.array(z.string()).optional(),
     }),
